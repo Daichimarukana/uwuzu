c3a5d1a87b5c362b629bd92ceefdd81c95fb39eb
だいちまる <98202777+Daichimarukana@users.noreply.github.com>
2023-08-17 19:59:43 +0900

    Delete db.php

--- db.php
+++
@@ -1,9 +0,0 @@
-<?php // データベースの接続情報
-define( 'DB_HOST', 'localhost');
-define( 'DB_USER', 'root');
-define( 'DB_PASS', 'root');
-define( 'DB_NAME', 'uwuzu');
-
-// タイムゾーン設定
-date_default_timezone_set('Asia/Tokyo');
-?>
