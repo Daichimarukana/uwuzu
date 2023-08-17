493c993b74ab06aa32a85800b688cc6cc88ba60e
だいちまる <98202777+Daichimarukana@users.noreply.github.com>
2023-08-17 19:57:42 +0900

    Delete js directory

--- js/back.js
+++
@@ -1,6 +0,0 @@
-
-window.onload = function(){
-var ele = document.getElementsByTagName("body")[0];
-var n = Math.floor(Math.random() * 3); // 3枚の画像がある場合
-ele.style.backgroundImage = "url(img/titleimg/"+n+".png)";
-}
